df743bcc35e7bcfb4f79b16e993ec5bf3ff3a612
nlohmill <nils.lohmiller@hs-esslingen.de>
2024-05-17 22:06:20 +0200

    First commit
    Co-authored-by: Markus <HCl-not-HCi@users.noreply.github.com>

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,8 +1,104 @@
 from typing import List
 from models.game_state import GameState
+from models.game_config import GameConfig, BaseLevel
 from models.player_action import PlayerAction
+from models.base import Base
+# import numpy as np
+import math
+
+minDefenders = 5
+def euclid(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int):
+    return math.floor(math.sqrt(((x1-x2)**2+(y1-y2)**2+(z1-z2)**2)))
+
+def get_base_distance(base_1: Base, base_2: Base) -> float:
+    return euclid(
+        base_1.position.x, base_1.position.y, base_1.position.z,
+        base_2.position.x, base_2.position.y, base_2.position.z
+    )
+
+def get_base_level(base: Base, base_levels: list[BaseLevel]) -> BaseLevel:
+    return base_levels[base.level]
+    
+
+def filter_bases(bases: list[Base], our_player: int) -> tuple[list[Base], list[Base], list[Base]]:
+    our_bases = []
+    other_bases = []
+    empty_bases = []
+
+    for base in bases:
+        if base.player == our_player:
+            our_bases.append(base)
+        elif base.player == 0:
+            empty_bases.append(base)
+        else:
+            other_bases.append(base)
+    
+    return (our_bases, other_bases, empty_bases)
+
+# def get_nearest_base(bases: list[Base], src_base: Base) -> Base:
+#     nearest_base = bases[0]
+    
+#     for base in bases:
+#         if get_nearest_base(src_base, base)
+
+def survivors(srcBase: Base, destBase: Base, config: GameConfig, bits: int) -> int:
+    grace_period = config.paths.grace_period
+    death_rate = config.paths.death_rate
+    deaths = max((get_base_distance(srcBase,destBase) - grace_period), 0) * death_rate
+    return max(bits - deaths, 0)
+
+def defendersAtTime(destTime: int, destBase: Base, config: GameConfig) -> int:
+    spawn_rate = get_base_level(destBase, config.base_levels).spawn_rate
+    return destBase.population+destTime*spawn_rate
+        
+def attackDecision(attackers: int, destBase: Base, config: GameConfig, destTime: int) -> int:
+    return attackers-defendersAtTime(destTime,destBase,config)
+
+def populationAverage(bases: list[Base]):
+    basesPopulation=0
+    for base in bases:
+        basesPopulation+=base.population
+
+    return basesPopulation/len(bases)
+
+def iterateBases(otherBases: list[Base], ourBases: list[Base], config: GameConfig):
+    bestTargetBase=None
+    mostSurvivors=0
+    for targetBase in otherBases:
+        sumSurvivors=0
+        for selectedBase in ourBases:
+            sumSurvivors+=survivors(selectedBase, targetBase, config, max(selectedBase.population-minDefenders,0)) #TODO wert minDefenders dynamisch generieren
+        if sumSurvivors>mostSurvivors:
+            mostSurvivors = sumSurvivors
+            bestTargetBase = targetBase
+    
+    return bestTargetBase
+
+def attack(otherBases: list[Base], ourBases: list[Base], config: GameConfig) -> list[PlayerAction]:
+    attackList: list[PlayerAction] = []
+    targetBase = iterateBases(otherBases, ourBases, config) 
+    if targetBase is not None:
+        for srcBase in ourBases:
+            attackList.append([srcBase.uid,targetBase.uid,srcBase.population-minDefenders])
+        return attackList
+    
+    return attackList
 
 
 def decide(gameState: GameState) -> List[PlayerAction]:
     # TODO: place your logic here
-    return [PlayerAction(0, 0, 0)]
+    
+    
+    # Our player
+    our_player = gameState.game.player
+
+    bases = gameState.bases
+
+    global minDefenders 
+    minDefenders = populationAverage(bases)/2
+    our_bases, other_bases, empty_bases = filter_bases(bases, our_player)
+    
+
+    return attack(other_bases,our_bases.append(empty_bases),gameState.config)
+
+    
--- logic/strategy_test.py
+++ logic/strategy_test.py
@@ -1,7 +1,7 @@
 import unittest
 import uuid
 
-from logic.strategy import decide
+from logic.strategy import decide, euclid, get_base_level, filter_bases, survivors, defendersAtTime
 from models.base import Base
 from models.base_level import BaseLevel
 from models.board_action import BoardAction
@@ -12,8 +12,48 @@ from models.player_action import PlayerAction
 from models.position import Position
 from models.progress import Progress
 
-
 class TestStrategy(unittest.TestCase):
+
+    our_player = 1
+
+    base_levels = [
+        BaseLevel({
+            "max_population": 20,
+            "upgrade_cost": 1000,
+            "spawn_rate": 1
+        }),
+        BaseLevel({
+            "max_population": 40,
+            "upgrade_cost": 1000,
+            "spawn_rate": 2
+        }),
+        BaseLevel({
+            "max_population": 80,
+            "upgrade_cost": 1000,
+            "spawn_rate": 3
+        })
+    ]
+
+    game_config = GameConfig({
+        "base_levels": base_levels,
+        "paths": PathsConfig({
+            "grace_period": 10,
+            "death_rate": 1
+        })
+    })
+
+    # game = Game({
+    #     'uid': uid,
+    #     'tick': tick,
+    #     'player_count': playerCount,
+    #     'remaining_players': remainingPlayers,
+    #     'player': player
+    # })
+    
+    enemy_bases = [Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 2, 0, 0), 
+                   Base.fromAttributes(Position.fromAttributes(7, 1, 2), 0, 3, 2, 0, 0), ]
+    our_bases = [Base.fromAttributes(Position.fromAttributes(4, 0, 1), 0, our_player, 2, 0, 0), 
+                 Base.fromAttributes(Position.fromAttributes(3, 0, 2), 0, our_player, 2, 0, 0), ]
 
     def test_decide(self):
         test_base = Base.fromAttributes(Position.fromAttributes(0, 0, 0), 0, 0, 0, 0, 0)
@@ -22,7 +62,59 @@ class TestStrategy(unittest.TestCase):
         want = PlayerAction(0, 0, 0)
 
         result = decide(test_game_state)
-        self.assertEqual(str(want), str(result))
+        self.assertEqual(str(want), str(result[0]))
+
+    def test_euclid(self):
+        want = 1
+        result = euclid(0, 0, 0, 1, 0, 0)
+        self.assertEqual(want, result)
+        
+        want = 10
+        result = euclid(7, 4, 3, 17, 6, 0)
+        self.assertEqual(want, result)
+    
+
+    def test_get_base_level(self):
+        want = self.base_levels[0]
+
+        base = Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, self.our_player, 2, 0, 0)
+        result = get_base_level(base, self.base_levels)
+
+        self.assertEqual(want, result)
+
+
+    def test_filter_bases(self):
+        result_our, result_other, result_empty = filter_bases(self.enemy_bases, self.our_player)
+        self.assertEqual(str(want), str(result_our))
+        self.assertEqual(str(want), str(result_other))
+        self.assertEqual(str(self.enemy_bases), str(result_empty))
+
+        want = self.our_bases
+        result = filter_bases(self.our_bases + self.enemy_bases, self.our_player)
+        self.assertEqual(str(want), str(self.our_bases))
+
+
+    def test_survivors(self):
+        bits = 10
+        our_base = Base.fromAttributes(Position.fromAttributes(18, 0, 1), 0, self.our_player, 2, 0, 0)
+        target_base = Base.fromAttributes(Position.fromAttributes(8, 0, 1), 0, 0, 2, 0, 0)
+        surv = survivors(our_base, target_base,self.game_config, bits)
+        self.assertEqual(bits, surv)
+        our_base = Base.fromAttributes(Position.fromAttributes(20, 0, 1), 0, self.our_player, 2, 0, 0)
+        surv = survivors(our_base, target_base,self.game_config, bits)
+        self.assertEqual(8, surv)
+
+
+    # def test_defendersAtTime(self):
+    #     base = Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 2, 0, 0)
+
+    #     want = 0
+    #     result = defendersAtTime(1, )
+
+    def test_attack(self):
+        pass
+        
+
 
 if __name__ == '__main__':
     unittest.main()
