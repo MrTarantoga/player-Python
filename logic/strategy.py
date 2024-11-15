f23c602f85da652f72dab234a10338cdf81f64d2
Markus Schramm <markus.schramm@hs-esslingen.de>
2024-11-15 21:26:22 +0100

    New stratey
    Co-authored-by: nils <AviciiAtCreamfields@users.noreply.github.com>
    Co-authored-by: goedicke95@gmail.com <goedicke95@gmail.com>
    Co-authored-by: Goetz <MrTarantoga@users.noreply.github.com>

--- logic/strategy.py
+++ logic/strategy.py
@@ -5,6 +5,7 @@ from models.player_action import PlayerAction
 from models.base import Base
 # import numpy as np
 import math
+import pandas as pd
 
 minDefenders = 5
 def euclid(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int):
@@ -54,6 +55,7 @@ def defendersAtTime(destTime: int, destBase: Base, config: GameConfig) -> int:
 def attackDecision(attackers: int, destBase: Base, config: GameConfig, destTime: int) -> int:
     return attackers-defendersAtTime(destTime,destBase,config)
 
+# Das muss weg
 def populationAverage(bases: list[Base]):
     basesPopulation=0
     for base in bases:
@@ -61,28 +63,67 @@ def populationAverage(bases: list[Base]):
 
     return basesPopulation/len(bases)
 
-def iterateBases(otherBases: list[Base], ourBases: list[Base], config: GameConfig):
-    bestTargetBase=None
-    mostSurvivors=0
+def getEnemyValues(otherBases: list[Base], config: GameConfig):
+    data = {}
+    enemyUid = []
+    enemyGrowthRate = []
+    enemyMaxPopulation = []
+    enemyPopulation = []
+
     for targetBase in otherBases:
-        sumSurvivors=0
-        for selectedBase in ourBases:
-            sumSurvivors+=survivors(selectedBase, targetBase, config, max(selectedBase.population-minDefenders,0)) #TODO wert minDefenders dynamisch generieren
-        if sumSurvivors>mostSurvivors:
-            mostSurvivors = sumSurvivors
-            bestTargetBase = targetBase
+        enemyUid.append(targetBase.uid)
+        enemyGrowthRate.append(config.base_levels[targetBase.level].spawn_rate)
+        enemyMaxPopulation.append(config.base_levels[targetBase.level].max_population)
+        enemyPopulation.append(targetBase.population)
+
+    data["index"] = enemyUid
+    data["growth_rate"] = enemyGrowthRate
+    data["max_population"] = enemyMaxPopulation
+    data["population"] = enemyPopulation
+
+    return data
+
+# Generate Array with ale enemies and the costs to conquer them
+def iterateBases(otherBases: list[Base], ourBases: list[Base], config: GameConfig) -> list[PlayerAction]:
+    bestTargetBase: list[PlayerAction]  = []
+
+    #allBases = pd.DataFrame(data)
+    data = getEnemyValues(otherBases, config)
+
+    for ourBase in ourBases:
+        enemydist = []
+        for targetBase in otherBases:
+            
+            #enemydist[targetBase.uid] = (get_base_distance(ourBase,targetBase) - config.paths.grace_period)
+            enemydist.append(get_base_distance(ourBase,targetBase))
+        data[ourBase.uid] = enemydist
+
+    allBases = pd.DataFrame(data)
+    allBases = allBases.set_index(["index", "growth_rate", "max_population", "population"])
+    allBases_distanceCosts = allBases.copy()
+    # add death_rate for euclidean distance to enemy
+    allBases_distanceCosts = (allBases_distanceCosts.loc[:] - config.paths.grace_period).clip(lower=0) * config.paths.death_rate
+
+    # add gain of enemy during travel (TODO: cap gain by max_population)
+    allBases_distanceCosts = allBases_distanceCosts.loc[:] + (allBases * allBases.index.get_level_values("growth_rate"))
+    # add population of enemy at travel start
+    allBases_distanceCosts = allBases_distanceCosts.loc[:] + allBases.index.get_level_values("population")
+
+    # search for all possible targets
+    for ourBase in ourBases:
+        possibleTargets = allBases_distanceCosts[ourBase.uid].sort_values(ascending=True).copy()
+        for possibleTargetIndex, possibleTarget in possibleTargets.items():
+            if possibleTarget < ourBase.population:
+                # add enemy as target
+                bestTargetBase.append([ourBase.uid, possibleTargetIndex[0], possibleTarget + 1])
+                # reduce population of our ally
+                ourBase.population -= possibleTarget + 1
+                # eliminate enemy from possible targets (next ally must attack other enemies)
+                allBases_distanceCosts.drop([possibleTargetIndex])
+            else:
+                break
     
     return bestTargetBase
-
-def attack(otherBases: list[Base], ourBases: list[Base], config: GameConfig) -> list[PlayerAction]:
-    attackList: list[PlayerAction] = []
-    targetBase = iterateBases(otherBases, ourBases, config) 
-    if targetBase is not None:
-        for srcBase in ourBases:
-            attackList.append([srcBase.uid,targetBase.uid,srcBase.population-minDefenders])
-        return attackList
-    
-    return attackList
 
 
 def decide(gameState: GameState) -> List[PlayerAction]:
@@ -97,7 +138,6 @@ def decide(gameState: GameState) -> List[PlayerAction]:
     #minDefenders = populationAverage(bases)/2
     our_bases, other_bases, empty_bases = filter_bases(bases, our_player)
     
-
-    return attack(other_bases, our_bases + empty_bases, gameState.config)
+    return iterateBases(other_bases, our_bases + empty_bases, gameState.config)
 
     
--- logic/strategy_test.py
+++ logic/strategy_test.py
@@ -1,12 +1,15 @@
 import unittest
 import uuid
 
-from logic.strategy import decide, euclid, get_base_level, filter_bases, survivors, defendersAtTime
+from logic.strategy import decide, euclid, get_base_level, filter_bases, survivors, defendersAtTime, iterateBases, getEnemyValues
 from models.base import Base
 from models.base_level import BaseLevel
 from models.board_action import BoardAction
 from models.game import Game
 from models.game_config import GameConfig, PathConfig
+from models.base_level import BaseLevel
+from models.base import Base
+
 from models.game_state import GameState
 from models.player_action import PlayerAction
 from models.position import Position
@@ -26,30 +29,29 @@ class TestStrategy(unittest.TestCase):
         )
 
     base_levels = [
-        BaseLevel({
-            "max_population": 20,
-            "upgrade_cost": 1000,
-            "spawn_rate": 1
-        }),
-        BaseLevel({
-            "max_population": 40,
-            "upgrade_cost": 1000,
-            "spawn_rate": 2
-        }),
-        BaseLevel({
-            "max_population": 80,
-            "upgrade_cost": 1000,
-            "spawn_rate": 3
-        })
+        BaseLevel(max_population=20,
+            upgrade_cost=1000,
+            spawn_rate=1
+        ),
+        BaseLevel(
+            max_population=40,
+            upgrade_cost=1000,
+            spawn_rate=2
+        ),
+        BaseLevel(
+            max_population=80,
+            upgrade_cost=1000,
+            spawn_rate=3
+        )
     ]
 
-    game_config = GameConfig({
-        "base_levels": base_levels,
-        "paths": PathsConfig({
-            "grace_period": 10,
-            "death_rate": 1
-        })
-    })
+    game_config = GameConfig(
+        base_levels=base_levels,
+        paths=PathConfig(
+            grace_period=10,
+            death_rate=1
+        )
+    )
 
     # game = Game({
     #     'uid': uid,
@@ -59,10 +61,18 @@ class TestStrategy(unittest.TestCase):
     #     'player': player
     # })
     
-    enemy_bases = [Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 2, 0, 0), 
-                   Base.fromAttributes(Position.fromAttributes(7, 1, 2), 0, 3, 2, 0, 0), ]
-    our_bases = [Base.fromAttributes(Position.fromAttributes(4, 0, 1), 0, our_player, 2, 0, 0), 
-                 Base.fromAttributes(Position.fromAttributes(3, 0, 2), 0, our_player, 2, 0, 0), ]
+    enemy_bases = [
+        # Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 2, 0, 0), 
+        # Base.fromAttributes(Position.fromAttributes(7, 1, 2), 0, 3, 2, 0, 0), 
+        Base(uid=0, name='Our Base', player=1, population=100, level=0, units_until_upgrade=1, position=Position(5, 0, 1)),
+        Base(uid=0, name='Our Base', player=1, population=100, level=0, units_until_upgrade=1, position=Position(7, 1, 2))
+    ]
+    our_bases = [
+        # Base.fromAttributes(Position.fromAttributes(4, 0, 1), 0, our_player, 2, 0, 0), 
+        # Base.fromAttributes(Position.fromAttributes(3, 0, 2), 0, our_player, 2, 0, 0),
+        Base(uid=0, name='Our Base', player=our_player, population=100, level=0, units_until_upgrade=1, position=Position(4, 0, 1)),
+        Base(uid=0, name='Our Base', player=our_player, population=100, level=0, units_until_upgrade=1, position=Position(3, 0, 2))
+    ]
 
     # def test_decide(self):
     #     test_base = Base.fromAttributes(Position.fromAttributes(0, 0, 0), 0, 0, 0, 0, 0)
@@ -86,7 +96,7 @@ class TestStrategy(unittest.TestCase):
     def test_get_base_level(self):
         want = self.base_levels[0]
 
-        base = Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, self.our_player, 2, 0, 0)
+        base = Base(uid=0, name='1', player=self.our_player, population=100, level=0, units_until_upgrade=1, position=Position(5, 0, 1))
         result = get_base_level(base, self.base_levels)
 
         self.assertEqual(want, result)
@@ -106,26 +116,57 @@ class TestStrategy(unittest.TestCase):
 
     def test_survivors(self):
         bits = 10
-        our_base = Base.fromAttributes(Position.fromAttributes(18, 0, 1), 0, self.our_player, 2, 0, 0)
-        target_base = Base.fromAttributes(Position.fromAttributes(8, 0, 1), 0, 0, 2, 0, 0)
-        surv = survivors(our_base, target_base,self.game_config, bits)
-        self.assertEqual(bits, surv)
-        our_base = Base.fromAttributes(Position.fromAttributes(20, 0, 1), 0, self.our_player, 2, 0, 0)
-        surv = survivors(our_base, target_base,self.game_config, bits)
+        # our_base = Base.fromAttributes(Position.fromAttributes(18, 0, 1), 0, self.our_player, 2, 0, 0)
+        our_base = Base(uid=0, name='Our Base', player=self.our_player, population=100, level=0, units_until_upgrade=1, position=Position(18, 0, 1))
+        # target_base = Base.fromAttributes(Position.fromAttributes(8, 0, 1), 0, 0, 2, 0, 0)
+        target_base = Base(uid=0, name='1', player=self.our_player, population=100, level=0, units_until_upgrade=1, position=Position(8, 0, 1))
+        # surv = survivors(our_base, target_base,self.game_config, bits)
+        # our_base = Base.fromAttributes(Position.fromAttributes(20, 0, 1), 0, self.our_player, 2, 0, 0)
+        surv = survivors(our_base, target_base, self.game_config, bits)
         self.assertEqual(8, surv)
 
 
     def test_defendersAtTime(self):
         # base_level 1 --> spawn_rate 2
         # population 20
-        base = Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 20, 1, 0)
+        # base = Base.fromAttributes(Position.fromAttributes(5, 0, 1), 0, 2, 20, 1, 0)
+        base = Base(uid=0, name='Our Base', player=self.our_player, population=100, level=0, units_until_upgrade=1, position=Position(5, 0, 1))
         want = 24
         result = defendersAtTime(2, base, self.game_config)
         self.assertEqual(want, result)
 
+    def test_get_enemy_values(self):
+        other_bases = [
+            Base(uid=0, name='A', player=1, population=100, level=0, units_until_upgrade=1, position=Position(5, 0, 1)),
+            Base(uid=1, name='B', player=1, population=100, level=0, units_until_upgrade=1, position=Position(5, 0, 1))
+        ]
+
+        enemy_values = getEnemyValues(other_bases, self.game_config)
+
+        expected = {
+            'index': [0, 1],
+            'growth_rate': [
+                self.game_config.base_levels[other_bases[0].level].spawn_rate,
+                self.game_config.base_levels[other_bases[1].level].spawn_rate
+            ],
+            'max_population': [
+                self.game_config.base_levels[other_bases[0].level].max_population,
+                self.game_config.base_levels[other_bases[1].level].max_population
+            ],
+            'population': [100, 100]
+        }
+
+        self.assertEqual(enemy_values, expected)
+
+    def test_interate_bases(self):
+        our_bases = [self.our_bases[0]]
+        other_bases = [self.enemy_bases[0]]
+        iterateBases(other_bases, our_bases, self.game_config)
+
     def test_attack(self):
-        pass
-        
+        other_bases = self.enemy_bases[1]
+        our_bases = self.our_bases[1]
+        # attack(other_bases, our_bases, self.game_config)
 
 
 
